2f7bf2f0da7efd57097cd51dc98dde53a76f0385
Kynedi <Kynedi.Paulson@trojans.dsu.edu>
2024-02-06 07:47:24 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class HashTable <TKey, TValue>
+{
+    private int capacity;
+    private int size;
+    private double loadFactor;
+    private KeyValuePair<TKey, TValue>[] buckets;
+
+    public HashTable(int initial = 10, double loadFactor = 0.75)
+    {
+        this.capacity = initial;
+        this.size = 0;
+        this.loadFactor = loadFactor;
+        this.buckets = new KeyValuePair<TKey, TValue>[capacity];
+    }
+    #region HashFunctions
+    private int Hash(TKey key, int i) //double hash function
+    {
+        return (key.GetHashCode() + i * HashSecond(key)) % capacity;
+    }
+    private int HashSecond(TKey key) //secondary hash function 
+    {
+        return 1 + (key.GetHashCode() % (capacity - 1));
+    }
+    #endregion
+    #region Resize
+    private void resize()
+    {
+        //Resize hash table when capacity exceeds threshold
+        int newCap = 2 * capacity;
+        KeyValuePair<TKey, TValue>[] newBuckets = new KeyValuePair<TKey, TValue>[newCap];
+
+        //Rehash existing pairs
+        for (int i = 0; i < capacity; i++)
+        {
+            var pair = buckets[i];
+            if (!pair.Equals(default(KeyValuePair<TKey, TValue>)))
+            {
+                int j = 0;
+                while (j < newCap)
+                {
+                    int index = Hash(pair.Key, j);
+                    if (newBuckets[index].Equals(default(KeyValuePair<TKey, TValue>)))
+                    {
+                        newBuckets[index] = pair;
+                        break;
+                    }
+                    j++;
+                }
+            }
+        }
+        capacity = newCap;
+        buckets = newBuckets;
+    }
+    #endregion
+    #region InsertPair
+    public void insert(TKey key, TValue value)
+    {
+        int i = 0;
+        while (i < capacity)
+        {
+            int index = Hash(key, i);
+            if (buckets[index].Equals(default((TKey, TValue))))
+            {
+                buckets[index] = new KeyValuePair<TKey, TValue>(key, value);
+                size++;
+
+                if ((double)size / capacity > loadFactor)
+                {
+                    resize();
+                }
+                return;
+            }
+            i++;
+        }
+    }
+    #endregion
+    #region GetValue
+    public TValue Get(TKey key)
+    {
+        int i = 0;
+        while (i < capacity)
+        {
+            int index = Hash(key, i);
+            var pair = buckets[index];
+            if (!pair.Equals(default(KeyValuePair<TKey, TValue>)) && pair.Key.Equals(key))
+            {
+                return pair.Value; 
+            }
+            i++;
+        }
+        return default(TValue);
+    }
+    #endregion
+    #region RemovePair
+    public void remove(TKey key)
+    {
+        int i = 0;
+        while (i < capacity)
+        {
+            int index = Hash(key, i);
+            var pair = buckets[index];
+            if (!pair.Equals(default(KeyValuePair<TKey, TValue>)) && pair.Key.Equals(key))
+            {
+                buckets[index] = default(KeyValuePair<TKey, TValue>);
+                size--;
+                return;
+            }
+            i++;
+        }
+    }
+    #endregion   
+    #region ConvertToString
+    public override string ToString()
+    {
+        List<string> items = new List<string>();
+        for (int i = 0; i < capacity; i++)
+        {
+            var pair = buckets[i];
+            if(!pair.Equals(default(KeyValuePair<TKey, TValue>)))
+            {
+                items.Add($"({pair.Key}, {pair.Value})");
+            }
+        }
+        return "{" + string.Join(", ", items) + "}";
+    }
+    #endregion
+}
+
+ 
+class Program
+{
+    static void Main()
+    {
+        var hashTable = new HashTable<string, string>();
+
+        ReadDataFromFile(hashTable, "C:\\Users\\mater\\OneDrive\\Desktop\\DSU\\Advanced Data Structures\\HashTables\\us-contacts.csv");
+
+        Console.WriteLine(hashTable);
+    }
+    static void ReadDataFromFile(HashTable<string, string> hashTable, string filepath)
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] fields = line.Split(',');
+                    if (fields.Length >= 2)
+                    {
+                        string key = fields[0].Trim();
+                        string value = fields[1].Trim();
+
+                        hashTable.insert(key, value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid data in file.");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error reading file");
+        }
+    }
+}
